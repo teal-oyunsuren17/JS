db6f6c25cfde709d914c92e0136aab80ea097886
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-01-10 18:04:45 +0800

    add at class

--- JS figma/blog-js-figma/src/Todos/todos.js
+++ JS figma/blog-js-figma/src/Todos/todos.js
@@ -1,10 +1,13 @@
+import "../App.css";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-export default function Todos({ message }) {
+export default function Todos() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
   const [error, setError] = useState("");
+  const [editing, setEditing] = useState();
+  const [editingTexts, setEditingTexts] = useState({});
 
   function textChange(e) {
     setText(e.target.value);
@@ -14,13 +17,20 @@ export default function Todos({ message }) {
     if (text === "") {
       setError("Utgaa bichne uu");
     } else {
-      const newTodo = {
-        text: text,
-        done: false,
-        id: uuidv4(),
-      };
-      const newTodos = [newTodo, ...todos];
-      setTodos(newTodos);
+      if (editing === undefined) {
+        const newTodo = {
+          text: text,
+          done: false,
+          id: uuidv4(),
+        };
+        const newTodos = [newTodo, ...todos];
+        setTodos(newTodos);
+      } else {
+        const newTodos = [...todos];
+        newTodos[editing].text = text;
+        setTodos(newTodos);
+        setEditing(undefined);
+      }
       setText("");
       setError("");
     }
@@ -34,44 +44,82 @@ export default function Todos({ message }) {
     }
   }
 
-  function handleDoneChange(id, e) {
+  function handleDoneChange(id) {
     const newTodos = [...todos];
     const index = newTodos.findIndex((todo) => todo.id === id);
     newTodos[index].done = !newTodos[index].done;
     setTodos(newTodos);
   }
 
-  function editingTodo(index) {
-    const newTodos = [...todos];
-    const editedText = prompt("Todo zasah", todos[index].text);
-    newTodos[index].text = editedText;
+  function editingTodo(id, index) {
+    const newEditingTexts = { ...editingTexts };
+    newEditingTexts[id] = todos[index].text;
+    setEditingTexts(newEditingTexts);
+  }
+
+  function handleEditingText(id, e) {
+    const newEditingTexts = { ...editingTexts };
+    newEditingTexts[id] = e.target.value;
+    setEditingTexts(newEditingTexts);
+  }
+
+  function updateEditing(index, id) {
+    const newTodos = { ...todos };
+    newTodos[index].text = editingTexts[id];
     setTodos(newTodos);
+
+    cancelEditing(id);
+  }
+
+  function cancelEditing(id) {
+    const newEditingTexts = { ...editingTexts };
+    newEditingTexts[id] = undefined;
+    setEditingTexts(newEditingTexts);
   }
 
   return (
-    <div>
+    <div className="todoMain">
       <input value={text} onChange={textChange} />
       <button onClick={addTodo}> Nemeh</button>
 
       {error && <div>Aldaa: {error}</div>}
-
       <ul>
-        {todos.map((todo, index) => (
-          <li
-            key={todo.id}
-            style={{ textDecoration: todo.done ? "line-through" : "none" }}
-          >
-            <input
-              type={"checkbox"}
-              onChange={(e) => handleDoneChange(todo.id, e)}
-            />
-            {todo.text}
-            {!todo.done && (
-              <button onClick={() => editingTodo(index)}>zasah</button>
-            )}
-            <button onClick={() => deleteTodo(index)}>ustgah</button>
-          </li>
-        ))}
+        {todos.map((todo, index) => {
+          return (
+            <li
+              className="list"
+              key={todo.id}
+              style={{ textDecoration: todo.done ? "line-through" : "none" }}
+            >
+              {editingTexts[todo.id] !== undefined ? (
+                <>
+                  <input
+                    value={editingTexts[todo.id]}
+                    onChange={(e) => handleEditingText(todo.id, e)}
+                  />
+                  <button onClick={() => updateEditing(index, todo.id)}>
+                    hadgalah
+                  </button>
+                  <button onClick={() => cancelEditing(todo.id)}>bolih</button>
+                </>
+              ) : (
+                <>
+                  <input
+                    type={"checkbox"}
+                    onChange={(e) => handleDoneChange(todo.id, e)}
+                  />
+                  {todo.text}
+                  {!todo.done && (
+                    <button onClick={() => editingTodo(todo.id, index)}>
+                      zasah
+                    </button>
+                  )}
+                  <button onClick={() => deleteTodo(index)}>ustgah</button>
+                </>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
